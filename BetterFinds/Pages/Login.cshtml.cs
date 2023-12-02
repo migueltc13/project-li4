568e9e540b587ed5122435a21b01f29c49591d50
migueltc13 <migueltc13@gmail.com>
2023-12-02 23:29:44 +0000

    Update sql con with DefaultConnection + error messages

--- BetterFinds/Pages/Login.cshtml.cs
+++ BetterFinds/Pages/Login.cshtml.cs
@@ -1,46 +1,65 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
-// using Microsoft.EntityFrameworkCore;
-// using System.Data;
 
 namespace BetterFinds.Pages
 {
     public class LoginModel : PageModel
     {
         [BindProperty]
-        public string? Username { get; set; }
+        public string Username { get; set; } = "";
 
         [BindProperty]
-        public string? Password { get; set; }
+        public string Password { get; set; } = "";
+
+        private readonly IConfiguration _configuration;
+        public LoginModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public void OnGet()
         {
             // Executed when the page is requested using an HTTP GET request
         }
         public Task<IActionResult> OnPostAsync()
         {
-            Console.WriteLine($"Username: {Username}");
-            Console.WriteLine($"Password: {Password}");
-            SqlConnection con = new SqlConnection("Server=localhost\\MSSQLSERVER01;Database=master;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=False;");
-            con.Open();
-            string query = "SELECT * FROM Users WHERE Username = @Username AND Password = @Password";
-            SqlCommand cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@Username", Username);
-            cmd.Parameters.AddWithValue("@Password", Password);
+            Console.WriteLine($"Username: {Username}"); // TODO: Remove this
+            Console.WriteLine($"Password: {Password}"); // TODO: Remove this
 
-            using (SqlDataReader reader = cmd.ExecuteReader())
+            string connectionString = _configuration.GetConnectionString("DefaultConnection") ?? "";
+            SqlConnection con = new SqlConnection(connectionString);
+
+            try
             {
-                // Console.WriteLine("Checking credentials: " + reader.Read());
+                con.Open();
+
+                string query = "SELECT * FROM Users WHERE Username = @Username AND Password = @Password";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                // Use parameters to avoid SQL injection
+                cmd.Parameters.AddWithValue("@Username", Username);
+                cmd.Parameters.AddWithValue("@Password", Password);
+
+                using SqlDataReader reader = cmd.ExecuteReader();
                 if (reader.Read())
                 {
-                    Console.WriteLine("Correct credentials");
+                    // Login successful. TODO: Store user ID in session cookie
                     return Task.FromResult<IActionResult>(RedirectToPage("/Index"));
                 }
                 else
                 {
-                    Console.WriteLine("Incorrect credentials");
-                    return Task.FromResult<IActionResult>(RedirectToPage("/Login"));
+                    ModelState.AddModelError(string.Empty, "Invalid username or password.");
+                    return Task.FromResult<IActionResult>(Page());
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return Task.FromResult<IActionResult>(RedirectToPage("/Login"));
+            }
+            finally
+            {
+                con.Close();
             }
         }
     }
