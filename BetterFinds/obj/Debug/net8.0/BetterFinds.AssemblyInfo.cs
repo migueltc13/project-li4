018ced7f3ce27f0424e05e88eceb561f599bbd06
migueltc13 <migueltc13@gmail.com>
2023-12-02 23:26:41 +0000

    Update project files Update obj, bin, .vs directories

--- BetterFinds/obj/Debug/net8.0/BetterFinds.AssemblyInfo.cs
+++ BetterFinds/obj/Debug/net8.0/BetterFinds.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BetterFinds")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f327872357d10174f941cf2e4bdae955619c8d83")]
 [assembly: System.Reflection.AssemblyProductAttribute("BetterFinds")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BetterFinds")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
