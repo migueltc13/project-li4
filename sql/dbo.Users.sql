cfd746ef1ffb0332dc63b6863ee06f69d3fc701b
migueltc13 <migueltc13@gmail.com>
2023-12-02 23:19:43 +0000

    Add Users table

---
+++ sql/dbo.Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]       INT            NOT NULL,
+    [Username] NVARCHAR (MAX) NOT NULL,
+    [Email]    NVARCHAR (MAX) NOT NULL,
+    [Password] NVARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
