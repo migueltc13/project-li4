214c4c2d79b129af68e3f9e6fcc02a4ab42a3aa6
migueltc13 <migueltc13@gmail.com>
2023-12-04 23:59:52 +0000

    Create /Logout page

---
+++ BetterFinds/Pages/Logout.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BetterFinds.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGet()
+        {
+            Console.WriteLine("Logging out"); // TODO: Remove this
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/Login");
+        }
+    }
+}
