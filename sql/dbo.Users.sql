9a65ab04639f4d8b986614169c9d792245b4e2eb
migueltc13 <migueltc13@gmail.com>
2023-12-05 03:20:23 +0000

    Add FullName and OptNewsletter to Users table

--- sql/dbo.Users.sql
+++ sql/dbo.Users.sql
@@ -1,7 +1,9 @@
 ﻿CREATE TABLE [dbo].[Users] (
     [Id]       INT            NOT NULL,
+	[FullName] NVARCHAR (MAX) NOT NULL,
     [Username] NVARCHAR (MAX) NOT NULL,
     [Email]    NVARCHAR (MAX) NOT NULL,
     [Password] NVARCHAR (MAX) NOT NULL,
+    [OptNewsletter] BIT NOT NULL DEFAULT 0, 
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
