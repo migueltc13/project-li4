1e436d1776d14a868d00623e01953fe9a57670c5
migueltc13 <migueltc13@gmail.com>
2023-12-19 22:29:21 +0000

    Create auction page - view a single auction by id

---
+++ BetterFinds/Pages/Auction.cshtml.cs
@@ -0,0 +1,115 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace BetterFinds.Pages
+{
+    public class AuctionModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+        public AuctionModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult OnGet()
+        {
+            if (!int.TryParse(HttpContext.Request.Query["id"], out int auctionId))
+            {
+                return NotFound();
+            }
+
+            // debug
+            Console.WriteLine($"Auction id requested: {auctionId}");
+
+            string connectionString = _configuration.GetConnectionString("DefaultConnection") ?? "";
+            SqlConnection con = new SqlConnection(connectionString);
+
+            try
+            {
+                con.Open();
+
+                string query = "SELECT * FROM Auction WHERE AuctionId = @AuctionId";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@AuctionId", auctionId);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        DateTime startTime = reader.GetDateTime(reader.GetOrdinal("StartTime"));
+                        DateTime endTime = reader.GetDateTime(reader.GetOrdinal("EndTime"));
+                        int userId = reader.GetInt32(reader.GetOrdinal("UserId"));
+                        int productId = reader.GetInt32(reader.GetOrdinal("ProductId"));
+
+                        // Values to be used in the cshtml page
+                        ViewData["StartTime"] = startTime.ToString("yyyy-MM-dd HH:mm:ss");
+                        ViewData["EndTime"] = endTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+                        reader.Close();
+
+                        // Get seller username (NOTE no exections are being made)
+                        string fullName = string.Empty;
+                        string username = string.Empty;
+                        string queryUser = "SELECT FullName, Username FROM Users WHERE UserId = @UserId";
+                        SqlCommand cmdUser = new SqlCommand(queryUser, con);
+                        cmdUser.Parameters.AddWithValue("@UserId", userId);
+                        using (SqlDataReader readerUser = cmdUser.ExecuteReader())
+                        {
+                            if (readerUser.Read())
+                            {
+                                fullName = readerUser.GetString(readerUser.GetOrdinal("FullName"));
+                                username = readerUser.GetString(readerUser.GetOrdinal("Username"));
+                            }
+                            readerUser.Close();
+                        }
+
+                        ViewData["FullName"] = fullName;
+                        ViewData["Username"] = username;
+
+                        // Get Product info: name, description and price
+                        string productName = string.Empty;
+                        string productDesc = string.Empty;
+                        int productPrice = 0;
+                        string productQuery = "SELECT Name, Description, Price FROM Product WHERE ProductId = @ProductId";
+                        SqlCommand cmdProduct = new SqlCommand(productQuery, con);
+                        cmdProduct.Parameters.AddWithValue("@ProductId", productId);
+                        using (SqlDataReader readerProduct = cmdProduct.ExecuteReader())
+                        {
+                            if (readerProduct.Read())
+                            {
+                                productName = readerProduct.GetString(readerProduct.GetOrdinal("Name"));
+                                productDesc = readerProduct.GetString(readerProduct.GetOrdinal("Description"));
+                                productPrice = readerProduct.GetInt32(readerProduct.GetOrdinal("Price"));
+                            }
+                            readerProduct.Close();
+                        }
+
+                        ViewData["ProductName"] = productName;
+                        ViewData["ProductDesc"] = productDesc;
+                        ViewData["ProductPrice"] = ((double)productPrice / 100).ToString("0.00");
+
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                // return RedirectToPage("/auction");
+                return Page();
+            }
+            finally
+            {
+                con.Close();
+            }
+
+
+            return Page();
+        }
+    }
+}
