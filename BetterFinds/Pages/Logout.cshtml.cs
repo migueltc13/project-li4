a87593932416df66018fdf75a300eeec384a3f3b
migueltc13 <migueltc13@gmail.com>
2023-12-22 15:03:16 +0000

    Change redirect to /login after logging out

--- BetterFinds/Pages/Logout.cshtml.cs
+++ BetterFinds/Pages/Logout.cshtml.cs
@@ -11,7 +11,7 @@ namespace BetterFinds.Pages
         {
             Console.WriteLine("Logging out"); // TODO: Remove this
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToPage("/Login");
+            return RedirectToPage("/login");
         }
     }
 }
