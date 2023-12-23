996a80a1d35a78ade017b7a48b81cc6523cb7ca0
migueltc13 <migueltc13@gmail.com>
2023-12-23 01:26:04 +0000

    Replace TIME with DATETIME for Time field

--- sql/dbo.Bid.sql
+++ sql/dbo.Bid.sql
@@ -2,7 +2,7 @@
     [BidId]     INT           NOT NULL,
     [Value]     INT           NOT NULL,
     [Status]    VARCHAR (MAX) NOT NULL,
-    [Time]      TIME (7)      NOT NULL,
+    [Time]      DATETIME      NOT NULL,
     [ClientId]  INT           NOT NULL,
     [AuctionID] INT           NOT NULL,
     PRIMARY KEY CLUSTERED ([BidId] ASC)
