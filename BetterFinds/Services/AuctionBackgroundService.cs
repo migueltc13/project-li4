22a7c4ac8339b53a4c3f39ae248d49eb732d4291
migueltc13 <migueltc13@gmail.com>
2024-01-10 15:00:05 +0000

    Create AuctionBackgroundService.cs to check asynchronously for auctions ending times

---
+++ BetterFinds/Services/AuctionBackgroundService.cs
@@ -0,0 +1,28 @@
+﻿using BetterFinds.Utils;
+
+namespace BetterFinds.Services
+{
+    public class AuctionBackgroundService : BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public AuctionBackgroundService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                using (var scope = _serviceProvider.CreateScope())
+                {
+                    var auctions = scope.ServiceProvider.GetRequiredService<Auctions>();
+                    await auctions.CheckAuctionsAsync();
+                }
+
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+            }
+        }
+    }
+}
