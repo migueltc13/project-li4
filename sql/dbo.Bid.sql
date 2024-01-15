99fe1f8986aeb805caa81344b0b7f1facc06af16
migueltc13 <migueltc13@gmail.com>
2024-01-15 23:19:26 +0000

    Remove Status field from Bid table

--- sql/dbo.Bid.sql
+++ sql/dbo.Bid.sql
@@ -1,9 +1,8 @@
 ﻿CREATE TABLE [dbo].[Bid] (
-    [BidId]     INT           NOT NULL,
-    [Value]     INT           NOT NULL,
-    [Status]    VARCHAR (MAX) NOT NULL,
-    [Time]      DATETIME      NOT NULL,
-    [ClientId]  INT           NOT NULL,
-    [AuctionId] INT           NOT NULL,
+    [BidId]     INT      NOT NULL,
+    [Value]     INT      NOT NULL,
+    [Time]      DATETIME NOT NULL,
+    [ClientId]  INT      NOT NULL,
+    [AuctionId] INT      NOT NULL,
     PRIMARY KEY CLUSTERED ([BidId] ASC)
 );
