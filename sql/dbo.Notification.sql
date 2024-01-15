5988090017be4d92ac936a47b89cb76ab2766a8f
migueltc13 <migueltc13@gmail.com>
2024-01-15 23:20:25 +0000

    Create Notification table

---
+++ sql/dbo.Notification.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Notification]
+(
+	[NotificationId] INT NOT NULL PRIMARY KEY,
+	[Message] NVARCHAR(256) NOT NULL,
+	[Timestamp] DATETIME NOT NULL,
+	[ClientId] INT NOT NULL,
+	[AuctionId] INT NOT NULL,
+	[IsRead] BIT NOT NULL DEFAULT 0
+);
