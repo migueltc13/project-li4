b840e3254093e3b4fcbe45db7d9f3f2d6bef7e08
migueltc13 <migueltc13@gmail.com>
2024-01-15 23:51:41 +0000

    Changed Account page to display public profiles

--- BetterFinds/Pages/Account.cshtml.cs
+++ BetterFinds/Pages/Account.cshtml.cs
@@ -1,10 +1,9 @@
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
 
 namespace BetterFinds.Pages
 {
-    [Authorize]
     public class AccountModel : PageModel
     {
         private readonly IConfiguration _configuration;
@@ -13,29 +12,84 @@ namespace BetterFinds.Pages
             _configuration = configuration;
         }
 
-        public void OnGet()
+        public List<Dictionary<string, object>>? AuctionsList { get; set; }
+
+        public IActionResult OnGet()
         {
-            // Get ClientId
+            if (!int.TryParse(HttpContext.Request.Query["id"], out int clientId))
+            {
+                return NotFound();
+            }
+
+            // Get current client id to check if the user can edit the account,
+            // if so add edit option by redirecting to the current user my account page
             var clientUtils = new Utils.Client(_configuration);
-            int ClientId = clientUtils.GetClientId(HttpContext, User);
+            int CurrentClientId = clientUtils.GetClientId(HttpContext, User);
+            if (CurrentClientId == clientId)
+                ViewData["Edit"] = true;
 
-            // Get user info from database
-            string connectionString = _configuration.GetConnectionString("DefaultConnection") ?? "";
-            SqlConnection con = new SqlConnection(connectionString);
-            string query = "SELECT * FROM Client WHERE ClientId = @ClientId";
-            SqlCommand cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@ClientId", ClientId);
+            // Check if client exists
+            string? connectionString = _configuration.GetConnectionString("DefaultConnection");
+            string query = "SELECT COUNT(*) AS NumberOfClients FROM Client WHERE ClientId = @ClientId";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@ClientId", clientId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        if (reader.GetInt32(reader.GetOrdinal("NumberOfClients")) == 0)
+                            return NotFound();
+                    }
+                    reader.Close();
+                }
+                con.Close();
+            }
 
-            con.Open();
-            SqlDataReader reader = cmd.ExecuteReader();
-            reader.Read();
-            ViewData["Username"] = reader.GetString(reader.GetOrdinal("Username"));
-            ViewData["FullName"] = reader.GetString(reader.GetOrdinal("FullName"));
-            ViewData["Email"] = reader.GetString(reader.GetOrdinal("Email"));
-            ViewData["OptNewsletter"] = reader.GetBoolean(reader.GetOrdinal("OptNewsletter")) ? "Yes" : "No";
+            // Get client info: full name and username
+            query = "SELECT FullName, Username FROM Client WHERE ClientId = @ClientId";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
 
-            con.Close();
-            // TODO (optional) option to change account options
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@ClientId", clientId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        ViewData["FullName"] = reader.GetString(reader.GetOrdinal("FullName"));
+                        ViewData["Username"] = reader.GetString(reader.GetOrdinal("Username"));
+                    }
+                    reader.Close();
+                }
+                con.Close();
+            }
+
+            // Get list of auctions
+            var auctionsUtils = new Utils.Auctions(_configuration);
+            AuctionsList = auctionsUtils.GetAuctions(clientId: clientId, order: 0, reversed: false, occurring: false);
+
+            // Get number of bids
+            query = "SELECT COUNT(*) AS NumberOfBids FROM Bid WHERE ClientId = @ClientId";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@ClientId", clientId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                        ViewData["NumBids"] = reader.GetInt32(reader.GetOrdinal("NumberOfBids"));
+                    reader.Close();
+                }
+                con.Close();
+            }
+
+            return Page();
         }
     }
 }
