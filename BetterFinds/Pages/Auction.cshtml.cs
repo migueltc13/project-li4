eb96bc62c9da40d35d85a07f700edd7008302fd3
migueltc13 <migueltc13@gmail.com>
2024-01-16 00:29:33 +0000

    Auction.cshtml.cs: Add signalR notifications when a bid is made, bid history, ViewData for BuyerId SellerId and MinimumBid, remove bid status and change OnPost method to Async

--- BetterFinds/Pages/Auction.cshtml.cs
+++ BetterFinds/Pages/Auction.cshtml.cs
@@ -1,19 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.SignalR;
 
 namespace BetterFinds.Pages
 {
     public class AuctionModel : PageModel
     {
         private readonly IConfiguration _configuration;
-        public AuctionModel(IConfiguration configuration)
+        private readonly IHubContext<NotificationHub> _hubContext;
+        public AuctionModel(IConfiguration configuration, IHubContext<NotificationHub> hubContext)
         {
             _configuration = configuration;
+            _hubContext = hubContext;
         }
+        
         [BindProperty]
         public int BidAmount { get; set; } = 0;
-        public IActionResult OnPost()
+        public IActionResult OnPostAsync()
         {
             if (!int.TryParse(HttpContext.Request.Query["id"], out int auctionId))
             {
@@ -140,18 +144,49 @@ namespace BetterFinds.Pages
                 }
 
                 // Update Bid table
-                query = "INSERT INTO Bid (BidId, Value, Status, Time, ClientId, AuctionId) VALUES (@BidId, @Value, @Status, @Time, @ClientId, @AuctionId)";
+                query = "INSERT INTO Bid (BidId, Value, Time, ClientId, AuctionId) VALUES (@BidId, @Value, @Time, @ClientId, @AuctionId)";
                 using (SqlCommand cmd = new SqlCommand(query, con))
                 {
                     cmd.Parameters.AddWithValue("@BidId", BidId);
                     cmd.Parameters.AddWithValue("@Value", BidAmount * 100);
-                    cmd.Parameters.AddWithValue("@Status", "Active");
                     cmd.Parameters.AddWithValue("@Time", DateTime.Now);
                     cmd.Parameters.AddWithValue("@ClientId", ClientId);
                     cmd.Parameters.AddWithValue("@AuctionId", auctionId);
 
                     cmd.ExecuteNonQuery();
                 }
+
+                // Add the bidder to the bidder group
+                var bidsUtils = new Utils.Bids(_configuration);
+                bidsUtils.AddBidderToBidderGroup(ClientId, auctionId).Wait();
+
+                // Notification message
+                string message = $"A new bid has been placed on the amount of {BidAmount}€";
+
+                // Create notification for each bidder except the current one
+                var notificationUtils = new Utils.Notification(_configuration);
+                List<int> bidders = bidsUtils.GetBiddersFromAuction(auctionId);
+                foreach (int bidder in bidders)
+                {
+                    if (bidder != ClientId)
+                    {
+                        notificationUtils.CreateNotification(bidder, auctionId, message);
+                    }
+                }
+
+                // Refresh notifications count for all clients
+                // calculate number of unread messages for each client that is member of the bidders group
+                int notificationCount = 0;
+                for (int i = 0; i < bidders.Count; i++)
+                {
+                    notificationCount = notificationUtils.GetNUnreadMessages(bidders[i]);
+                    _hubContext.Clients.All.SendAsync("ReceiveNotificationCount", notificationCount, bidders[i]).Wait();
+                }
+                
+                // Refresh auction page for all clients located that page
+                _hubContext.Clients.All.SendAsync("UpdatePrice", ClientId, auctionId, message).Wait();
+
+                Console.WriteLine("Send notification to clients");
             }
 
             return OnGet();
@@ -163,8 +198,19 @@ namespace BetterFinds.Pages
                 return NotFound();
             }
 
-            // debug
-            Console.WriteLine($"Auction id requested: {auctionId}");
+            // Get current ClientId
+            var clientUtilsClientId = new Utils.Client(_configuration);
+            int currentClientId = clientUtilsClientId.GetClientId(HttpContext, User);
+            ViewData["CurrentClientId"] = currentClientId;
+
+            // Get the bidders group for the current auction
+            var bidsUtils = new Utils.Bids(_configuration);
+            List<int> biddersGroup = bidsUtils.GetBiddersFromAuction(auctionId);
+            ViewData["BiddersGroup"] = biddersGroup;
+
+            Console.WriteLine($"Auction id requested: {auctionId}"); // TODO remove this
+
+            ViewData["AuctionId"] = auctionId;
 
             string connectionString = _configuration.GetConnectionString("DefaultConnection") ?? "";
             SqlConnection con = new SqlConnection(connectionString);
@@ -192,6 +238,8 @@ namespace BetterFinds.Pages
                         // Values to be used in the cshtml page
                         ViewData["StartTime"] = startTime.ToString("yyyy-MM-dd HH:mm:ss");
                         ViewData["EndTime"] = endTime.ToString("yyyy-MM-dd HH:mm:ss");
+                        ViewData["MinimumBid"] = ((int)minimumBid / 100).ToString("0.00");
+                        ViewData["SellerId"] = clientId;
 
                         // Check if auction has ended
                         ViewData["AuctionEnded"] = (DateTime.Now >= endTime);
@@ -249,6 +297,7 @@ namespace BetterFinds.Pages
                             readerProduct.Close();
                         }
 
+                        ViewData["BuyerId"] = BuyerId;
                         ViewData["ProductName"] = productName;
                         ViewData["ProductDesc"] = productDesc;
                         ViewData["ProductPrice"] = ((decimal)productPrice / 100).ToString("0.00");
@@ -269,6 +318,43 @@ namespace BetterFinds.Pages
                                     ViewData["BuyerUsername"] = readerBuyer.GetString(readerBuyer.GetOrdinal("Username"));
                                 }
                                 readerBuyer.Close();
+                            }
+                        }
+
+                        // Get clients info to avoid multiple sql queries
+                        var clientUtils = new Utils.Client(_configuration);
+                        List<Dictionary<string, object>> clients = clientUtils.GetClients();
+
+                        // Get bids history
+                        List<Dictionary<string, object>> Bids = new List<Dictionary<string, object>>();
+                        query = "SELECT Value, Time, ClientId FROM Bid WHERE AuctionId = @AuctionId ORDER BY Time DESC"; // Check order by time
+                        using (SqlCommand cmdBids = new SqlCommand(query, con))
+                        {
+                            cmdBids.Parameters.AddWithValue("@AuctionId", auctionId);
+                            using (SqlDataReader readerBids = cmdBids.ExecuteReader())
+                            {
+                                while (readerBids.Read())
+                                {
+                                    Dictionary<string, object> Bid = new Dictionary<string, object>();
+                                    Bid["Value"] = ((decimal)readerBids.GetInt32(readerBids.GetOrdinal("Value")) / 100).ToString("0.00");
+                                    Bid["Time"] = readerBids.GetDateTime(readerBids.GetOrdinal("Time")).ToString("yyyy-MM-dd HH:mm:ss");
+
+                                    // Get bidder username using ClientId
+                                    int BidderId = readerBids.GetInt32(readerBids.GetOrdinal("ClientId"));
+                                    foreach (Dictionary<string, object> client in clients)
+                                    {
+                                        if ((int)client["ClientId"] == BidderId)
+                                        {
+                                            // Bid["FullName"] = client["FullName"];
+                                            Bid["Username"] = client["Username"];
+                                            Bid["BidderId"] = BidderId;
+                                        }
+                                    }
+                                    
+                                    Bids.Add(Bid);
+                                }
+
+                                ViewData["Bids"] = Bids;
                             }
                         }
                     }
