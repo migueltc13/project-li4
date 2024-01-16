7f1ba091c753539405817c6b4a07f4905892418b
migueltc13 <migueltc13@gmail.com>
2024-01-16 16:55:45 +0000

    Enhance Notifications page: ShowAll, MarkAsRead and MarkAllAsRead

--- BetterFinds/Pages/Notifications.cshtml.cs
+++ BetterFinds/Pages/Notifications.cshtml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BetterFinds.Pages
@@ -14,18 +15,50 @@ namespace BetterFinds.Pages
 
         public List<Dictionary<string, object>> Notifications = new();
 
-        public void OnGet()
+        public bool ShowAll = false;
+
+        public IActionResult OnGet()
         {
             // Get ClientId
             var clientUtils = new Utils.Client(_configuration);
             int clientId = clientUtils.GetClientId(HttpContext, User);
+            
+            // Notification utils
+            var notificationUtils = new Utils.Notification(_configuration);
+
+            // Option to mark a notification as read: ?MarkAsRead=NofiticationId
+            if (Request.Query.TryGetValue("MarkAsRead", out var markAsReadValue) && !string.IsNullOrEmpty(markAsReadValue))
+            {
+                if (int.TryParse(markAsReadValue, out int notificationId))
+                {
+                    notificationUtils.MarkAsRead(clientId, notificationId);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+
+            // Option to mark all notifications as read: ?MarkAllAsRead=1
+            if (Request.Query.ContainsKey("MarkAllAsRead"))
+            {
+                notificationUtils.MarkAllAsRead(clientId);
+            }
+
+            // Toggle between unread and all notifications: ?ShowAll=1 or nothing
+            if (Request.Query.ContainsKey("ShowAll"))
+            {
+                Notifications = notificationUtils.GetNotifications(clientId);
+                ShowAll = Request.Query["ShowAll"] == "1";
+            }
 
             // Get notifications
-            var notificationUtils = new Utils.Notification(_configuration);
             Notifications = notificationUtils.GetNotifications(clientId);
 
-            // Mark notifications as read
-            notificationUtils.MarkAsRead(clientId);
+            // Get number of unread messages
+            ViewData["NUnreadMessages"] = notificationUtils.GetNUnreadMessages(clientId);
+
+            return Page();
         }
     }
 }
