c15e4d131d31313620670c99837d0bb456e596bb
migueltc13 <migueltc13@gmail.com>
2024-01-17 13:14:42 +0000

    Utils/Currency.cs: Create currency utils to convert decimal prices into strings

---
+++ BetterFinds/Utils/Currency.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace BetterFinds.Utils
+{
+    public class Currency
+    {
+        public static string FormatDecimal(decimal price)
+        {
+            NumberFormatInfo nfi = new()
+            {
+                NumberDecimalSeparator = "."
+            };
+
+            return price.ToString("0.00", nfi);
+        }
+
+        public static string FormatDecimalObject(object? price)
+        {
+            return FormatDecimal((decimal?)price ?? 0);
+        }
+    }
+}
