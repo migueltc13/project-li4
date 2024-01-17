ae66e782ef721ff3296380d4bd3e551e3235493f
migueltc13 <migueltc13@gmail.com>
2024-01-17 21:45:25 +0000

    Add IsCheckHasEnded field to Auction table

--- sql/dbo.Auction.sql
+++ sql/dbo.Auction.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[Auction] (
-    [AuctionId]   INT      NOT NULL,
-    [StartTime]   DATETIME NOT NULL,
-    [EndTime]     DATETIME NOT NULL,
-    [ClientId]    INT      NOT NULL,
-    [ProductId]   INT      NOT NULL,
-    [MinimumBid]  MONEY    NOT NULL,
-    [IsCompleted] BIT      DEFAULT ((0)) NOT NULL,
+    [AuctionId]       INT           NOT NULL,
+    [StartTime]       DATETIME      NOT NULL,
+    [EndTime]         DATETIME      NOT NULL,
+    [ClientId]        INT           NOT NULL,
+    [ProductId]       INT           NOT NULL,
+    [MinimumBid]      MONEY         NOT NULL,
+	[IsCheckHasEnded] BIT           DEFAULT ((0)) NOT NULL,
+    [IsCompleted]     BIT           DEFAULT ((0)) NOT NULL,
+    [PaymentMethod]   NVARCHAR (32) NULL,
     PRIMARY KEY CLUSTERED ([AuctionId] ASC)
 );
