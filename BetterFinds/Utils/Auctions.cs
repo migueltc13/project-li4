baae912d03305803246e907b6f02b89b648b7e5d
migueltc13 <migueltc13@gmail.com>
2024-01-17 21:54:13 +0000

    Utils/Auctions.cs: Handle auctions that have ended async

--- BetterFinds/Utils/Auctions.cs
+++ BetterFinds/Utils/Auctions.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Data.SqlClient;
+using System.Reflection;
 
 namespace BetterFinds.Utils
 {
     public class Auctions
     {
         private readonly IConfiguration _configuration;
-        public Auctions(IConfiguration configuration)
+        private readonly IHubContext<NotificationHub> _hubContext;
+        public Auctions(IConfiguration configuration, IHubContext<NotificationHub> hubContext)
         {
             _configuration = configuration;
+            _hubContext = hubContext;
         }
 
         /* Function to show auctions with the following parameters options
@@ -173,7 +177,6 @@ namespace BetterFinds.Utils
 
         public void AddAuction(DateTime endTime) => AuctionEndTimes.Add(endTime);
 
-        // TODO: Remove auction from AuctionEndTimes list when the auction is completed
         public void RemoveAuction(DateTime endTime) => AuctionEndTimes.Remove(endTime);
 
         public async Task CheckAuctionsAsync()
@@ -189,9 +192,120 @@ namespace BetterFinds.Utils
                         if (currentTime >= AuctionEndTimes[i])
                         {
                             Console.WriteLine($"AuctionEndTimes[{i}] has ended");
-                            // TODO: If there's an auction buyer
-                            // then notify buyer to buy the product (when buyer completes the purchase notify the seller)
-                            // else notify the seller
+
+                            int auctionId; // Retrieve auctionId from Auction table
+                            int sellerId; // Retrieve sellerId from Auction table
+                            int buyerId; // Retrieve buyerId from Product table
+                            string? conString = _configuration.GetConnectionString("DefaultConnection");
+                            using (SqlConnection con = new SqlConnection(conString))
+                            {
+                                con.Open();
+                                string query = "SELECT AuctionId, ClientId FROM Auction WHERE EndTime = @EndTime AND IsCheckHasEnded = 0";
+                                using (SqlCommand cmd = new SqlCommand(query, con))
+                                {
+                                    cmd.Parameters.AddWithValue("@EndTime", AuctionEndTimes[i]);
+                                    using (SqlDataReader reader = cmd.ExecuteReader())
+                                    {
+                                        if (!reader.Read())
+                                        {
+                                            break;
+                                        }
+
+                                        auctionId = reader.GetInt32(reader.GetOrdinal("AuctionId"));
+                                        sellerId = reader.GetInt32(reader.GetOrdinal("ClientId"));
+                                        reader.Close();
+                                    }
+                                }
+
+                                // Retrive buyerId from Product table
+                                query = "SELECT ClientId FROM Product WHERE AuctionId = @AuctionId";
+                                using (SqlCommand cmd = new SqlCommand(query, con))
+                                {
+                                    cmd.Parameters.AddWithValue("@AuctionId", auctionId);
+                                    using (SqlDataReader reader = cmd.ExecuteReader())
+                                    {
+                                        reader.Read();
+                                        buyerId = reader.GetInt32(reader.GetOrdinal("ClientId"));
+                                        reader.Close();
+                                    }
+                                }
+
+                                con.Close();
+                            }
+
+                            // Notifications
+                            string message;
+                            var notificationUtils = new Notification(_configuration);
+
+                            var bidsUtils = new Bids(_configuration);
+                            List<int> bidders = bidsUtils.GetBiddersFromAuction(auctionId);
+                            int notificationCount = 0;
+
+                            // check if buyerId is == 0 meaning there's no buyer
+                            if (buyerId == 0)
+                            {
+                                message = "Your auction has ended and no bids were made.";
+                                notificationUtils.CreateNotification(sellerId, auctionId, message);
+                                notificationCount = notificationUtils.GetNUnreadMessages(sellerId);
+                                Console.WriteLine($"Seller: {sellerId} - Auction: {auctionId} - notificationCount: {notificationCount}");
+                                _hubContext.Clients.All.SendAsync("ReceiveNotificationCount", notificationCount, sellerId).Wait();
+                            }
+                            else
+                            {
+                                // Notify seller that the auction has ended
+                                message = "Your auction has ended, waiting for the buyer to complete the purchase.";
+                                notificationUtils.CreateNotification(sellerId, auctionId, message);
+                                notificationCount = notificationUtils.GetNUnreadMessages(sellerId);
+                                Console.WriteLine($"Seller: {sellerId} - Auction: {auctionId} - notificationCount: {notificationCount}");
+                                _hubContext.Clients.All.SendAsync("ReceiveNotificationCount", notificationCount, sellerId).Wait();
+
+                                // Notify bidders except the buyer that the auction has ended
+                                message = "Auction has ended.";
+                                foreach (int bidder in bidders)
+                                {
+                                    if (bidder != buyerId)
+                                    {
+                                        notificationUtils.CreateNotification(bidder, auctionId, message);
+                                        notificationCount = notificationUtils.GetNUnreadMessages(bidder);
+                                        Console.WriteLine($"Bidder: {bidder} - Auction: {auctionId} - notificationCount: {notificationCount}");
+                                        _hubContext.Clients.All.SendAsync("ReceiveNotificationCount", notificationCount, bidder).Wait();
+                                    }
+                                }
+                                
+                                // Notify buyer that he won the auction and needs to complete the purchase
+                                message = "Auction has ended and you are the winner, please go to the auction page to complete the purchase.";
+                                notificationUtils.CreateNotification(buyerId, auctionId, message);
+                                notificationCount = notificationUtils.GetNUnreadMessages(buyerId);
+                                Console.WriteLine($"Buyer: {buyerId} - Auction: {auctionId} - notificationCount: {notificationCount}");
+                                _hubContext.Clients.All.SendAsync("ReceiveNotificationCount", notificationCount, buyerId).Wait();
+                            }
+
+                            // Update notification count for the bidders (including the buyer)
+                            foreach (int bidder in bidders)
+                            {
+                                
+                            }
+
+                            // Update notification count for the seller
+                            notificationCount = notificationUtils.GetNUnreadMessages(sellerId);
+                            Console.WriteLine($"Seller: {sellerId} - Auction: {auctionId} - notificationCount: {notificationCount}");
+                            _hubContext.Clients.All.SendAsync("ReceiveNotificationCount", notificationCount, sellerId).Wait();
+
+                            // Mark the auction IsCheckHasEnded as true
+                            using (SqlConnection con = new SqlConnection(conString))
+                            {
+                                con.Open();
+                                string query = "UPDATE Auction SET IsCheckHasEnded = 1 WHERE AuctionId = @AuctionId";
+                                using (SqlCommand cmd = new SqlCommand(query, con))
+                                {
+                                    cmd.Parameters.AddWithValue("@AuctionId", auctionId);
+                                    cmd.ExecuteNonQuery();
+                                }
+                                con.Close();
+                            }
+                            
+                            // Remove auction from AuctionEndTimes list
+                            AuctionEndTimes.RemoveAt(i);
                         }
                     }
                 });
