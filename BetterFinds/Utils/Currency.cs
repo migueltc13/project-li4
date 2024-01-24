71de6d1697682aff43c8c0e8d7ffdbb9934ec55e
migueltc13 <migueltc13@gmail.com>
2024-01-24 10:33:23 +0000

    Update Utils/Currency.cs doc

--- BetterFinds/Utils/Currency.cs
+++ BetterFinds/Utils/Currency.cs
@@ -1,33 +1,37 @@
-﻿using System.Globalization;
-
-namespace BetterFinds.Utils;
-
-/// <summary>
+﻿/// <summary>
 /// Provides utility functions for handling currency-related operations.
 /// </summary>
-public class Currency
+using System.Globalization;
+
+namespace BetterFinds.Utils
 {
     /// <summary>
-    /// Formats a decimal value as a string with two decimal places.
+    /// Provides utility functions for handling currency-related operations.
     /// </summary>
-    /// <param name="price">The decimal value to format.</param>
-    /// <returns>A string representation of the formatted decimal value.</returns>
-    public static string FormatDecimal(decimal price)
+    public class Currency
     {
-        NumberFormatInfo nfi = new()
+        /// <summary>
+        /// Formats a decimal value as a string with two decimal places.
+        /// </summary>
+        /// <param name="price">The decimal value to format.</param>
+        /// <returns>A string representation of the formatted decimal value.</returns>
+        public static string FormatDecimal(decimal price)
         {
-            NumberDecimalSeparator = "."
-        };
+            NumberFormatInfo nfi = new()
+            {
+                NumberDecimalSeparator = "."
+            };
 
-        return price.ToString("0.00", nfi);
+            return price.ToString("0.00", nfi);
+        }
+
+        /// <summary>
+        /// Formats an object representing a decimal value as a string with two decimal places.
+        /// If the provided object is null, it defaults to formatting zero.
+        /// </summary>
+        /// <param name="price">The object representing the decimal value to format.</param>
+        /// <returns>A string representation of the formatted decimal value.</returns>
+        public static string FormatDecimalObject(object? price) =>
+            FormatDecimal((decimal?)price ?? 0);
     }
-
-    /// <summary>
-    /// Formats an object representing a decimal value as a string with two decimal places.
-    /// If the provided object is null, it defaults to formatting zero.
-    /// </summary>
-    /// <param name="price">The object representing the decimal value to format.</param>
-    /// <returns>A string representation of the formatted decimal value.</returns>
-    public static string FormatDecimalObject(object? price) =>
-        FormatDecimal((decimal?)price ?? 0);
 }
