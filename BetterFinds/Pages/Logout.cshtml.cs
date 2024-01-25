fe6388c3275e0083191c1991057fc42cbe031c6c
migueltc13 <migueltc13@gmail.com>
2024-01-25 17:29:03 +0000

    Generate doc for Logout model

--- BetterFinds/Pages/Logout.cshtml.cs
+++ BetterFinds/Pages/Logout.cshtml.cs
@@ -3,14 +3,25 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BetterFinds.Pages;
-
-public class LogoutModel : PageModel
+namespace BetterFinds.Pages
 {
-    public async Task<IActionResult> OnGet()
+    /// <summary>
+    /// Model for the logout page.
+    /// </summary>
+    public class LogoutModel : PageModel
     {
-        Console.WriteLine($"User @{User?.Identity?.Name} logged out.");
-        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        return RedirectToPage("login", new { logout = 1 });
+        /// <summary>
+        /// Logs the user out and redirects them to the login page.
+        /// </summary>
+        /// <remarks>
+        /// This method is called when the user navigates to the logout page.
+        /// It logs the user out and redirects them to the login page with a logout message.
+        /// <returns>A asynchronous task that represents the logout operation.</returns>
+        public async Task<IActionResult> OnGet()
+        {
+            Console.WriteLine($"User @{User?.Identity?.Name} logged out.");
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("login", new { logout = 1 });
+        }
     }
 }
