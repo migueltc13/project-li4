c3061dcefed3f7cdc5773a170dd98e1ef4421236
flaviaraujo <flaviaraujo28@gmail.com>
2024-01-26 17:56:38 +0000

    Error documentation page

--- BetterFinds/Pages/Error.cshtml.cs
+++ BetterFinds/Pages/Error.cshtml.cs
@@ -2,18 +2,36 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace BetterFinds.Pages;
-
-[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-[IgnoreAntiforgeryToken]
-public class ErrorModel() : PageModel
+namespace BetterFinds.Pages
 {
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-    public void OnGet()
+    /// <summary>
+    /// The Model for the Error page.
+    /// This class is decorated with the ResponseCache 
+    /// and IgnoreAntiforgeryToken attributes.
+    /// </summary>
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
+    public class ErrorModel : PageModel
     {
-        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        /// <summary>
+        /// The request ID.
+        /// </summary>
+        public string? RequestId { get; set; }
+
+        /// <summary>
+        /// A boolean indicating whether or not the request ID should be shown.
+        /// </summary>
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        /// <summary>
+        /// Shows the error page.
+        /// </summary>
+        /// <remarks>
+        /// This page shows the error page with the request ID, if available.
+        /// </remarks>
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
     }
 }
