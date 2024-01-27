d9dcc532318b6524010a74eaa640ab7b8b7eeaee
migueltc13 <migueltc13@gmail.com>
2024-01-27 16:42:52 +0000

    Update Utils/Sql.cs functions

--- Tests/Utils/Sql.cs
+++ Tests/Utils/Sql.cs
@@ -30,7 +30,32 @@ namespace Tests.Utils
             try
             {
                 con.Open();
-                using SqlCommand command = new("SELECT MAX(AuctionId) FROM Auctions", con);
+                using SqlCommand command = new("SELECT MAX(AuctionId) FROM Auction", con);
+                using SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    result = reader.GetInt32(0);
+                    reader.Close();
+                    con.Close();
+                    return result;
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+
+        public static int GetClientIdByUsername(string username)
+        {
+            int result = 0;
+            using SqlConnection con = new(GetConnection());
+            try
+            {
+                con.Open();
+                using SqlCommand command = new($"SELECT ClientId FROM Client WHERE Username = '{username}'", con);
                 using SqlDataReader reader = command.ExecuteReader();
                 if (reader.Read())
                 {
